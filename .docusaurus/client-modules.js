f5db84c3a69bf0da401e5c90fe30eb34da796640
burhanudin hakim <udnpico@gmail.com>
2020-10-26 12:37:56 +0700

    delete folder

--- .docusaurus/client-modules.js
+++
@@ -1,6 +0,0 @@
-export default [
-  require("infima/dist/css/default/default.css"),
-  require("remark-admonitions/styles/infima.css"),
-  require("/Users/ruby/Desktop/start/support/src/css/custom.css"),
-  require("/Users/ruby/Desktop/start/support/node_modules/@docusaurus/theme-classic/src/prism-include-languages"),
-];
